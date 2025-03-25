0b706ab37def48fd0a24589d9167af510d505f72
Nikolay <75427338+korhenon@users.noreply.github.com>
2025-03-25 23:51:42 +0300

    Setup splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // Splash screen Android 12 API
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
--- app/src/main/java/com/example/languageapp/MainActivity.kt
+++ app/src/main/java/com/example/languageapp/MainActivity.kt
@@ -11,11 +11,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.example.languageapp.ui.theme.LanguageAppTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splash = installSplashScreen()
+        var keepSplashVisible = true
         super.onCreate(savedInstanceState)
+        splash.setKeepOnScreenCondition { keepSplashVisible }
+        lifecycleScope.launch {
+            // TODO: Add normal logic for splash screen
+            delay(5000)
+            keepSplashVisible = false
+        }
         enableEdgeToEdge()
         setContent {
             LanguageAppTheme {
